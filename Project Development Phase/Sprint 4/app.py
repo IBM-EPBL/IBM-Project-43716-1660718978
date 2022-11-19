e652419c653d10cd391cd6172dba690188bd50b8
exson6969 <amyexson@gmail.com>
2022-11-19 09:11:20 +0530

    update

--- Project Development Phase/Sprint 4/app.py
+++ Project Development Phase/Sprint 4/app.py
@@ -1,35 +1,51 @@
+from flask import Flask, render_template,request, jsonify
+from flask_cors import CORS
+import sklearn
 import numpy as np
-from flask import Flask, request, jsonify, render_template
+from sklearn.linear_model import LogisticRegression
 import pickle
-#import inputScript
+import phishLogic
 
 app = Flask(__name__)
-model = pickle.load(open('Phishing_Website.pkl','rb'))
+CORS(app)
+
+model = pickle.load(open("C:/Users/exson/Projects/IBM-Project-43716-1660718978/Project Development Phase/Sprint 4/Phishing_model.pkl","rb"))
+
+@app.route('/')
 
 @app.route('/predict')
 def predict():
-    return render_template('final.html')
+    return render_template('index.html')
 
-@app.route('/ypredict', methods = ['POST'])
-def y_predict():
-    url = request.form['URL']
-    checkprediction = inputScript.main(url)
-    print(prediction)
-    output =prediction[0]
-    if(output == 1):
-        pred = "You are safe and it's a legitmate Website."
-    else:
-        pred = "You are on the wrong site. Bc cautious!"
-    return trnder_template('final.html', prediction_text = '{}'. format(pred), url = url)
+@app.route('/y_predict',methods=['POST'])
+def y_predict(tempData):
+    
+    # url = request.form["url"]
+    url = tempData
+    checkprediction = phishLogic.main(url)
+    print(checkprediction)
+    return checkprediction
+    # prediction = model.predict(checkprediction)
+    # print(prediction)
+    # output=prediction[0]
+    # if(output==1):
+    #     pred="Your are safe!!  This is a Legitimate Website."
+    # else:
+    #     pred="You are on the wrong site. Be cautious!"
+    # return render_template('index.html', prediction_text='{}'.format(pred),url=url)
+    
 
-
-@app.route('/predict_api', methods = ['POST'])
+@app.route('/predict_api',methods=['POST'])
 def predict_api():
     data = request.get_json(force = True)
-    prediction = model.y_prdict([np.array(list(data.values()))])
+    print(data['url'])
+    tempData = data['url']
     
+    # prediction = y_predict(tempData)
+    prediction = phishLogic.main(tempData)
+    print(f"prediction {prediction}")
     output = prediction[0]
     return jsonify(output)
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', debug = True)
+    app.run(debug=True, host='0.0.0.0')
